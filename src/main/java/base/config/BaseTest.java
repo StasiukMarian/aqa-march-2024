3b026a0ec90071c5e178c402e9ceb7d2bc1bf8a1
Marian Stasiuk <mst@primexm.com>
2024-12-16 21:18:33 +0200

    lesson 3 work with elements

--- src/main/java/base/config/BaseTest.java
+++ src/main/java/base/config/BaseTest.java
@@ -1,46 +1,40 @@
 package base.config;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.testng.annotations.*;
 
 public class BaseTest {
-    @BeforeGroups
-    public void beforeGroup() {
-        System.out.println("This method works before group");
-    }
-
     @BeforeClass
     public void configuration() {
-        System.out.println("this is before class");
         Configuration.browser = "chrome";
         Configuration.browserSize = "1280x920";
-        Configuration.holdBrowserOpen = false;
+        Configuration.holdBrowserOpen = true;
         Configuration.timeout = 10000;
         Configuration.pageLoadTimeout = 10000;
         Configuration.headless = false;
         Configuration.screenshots = true;
         Configuration.savePageSource = false;
-
     }
 
     @BeforeMethod
     public void login() {
-        System.out.println("This method works before each test method");
+        // https://www.stats.govt.nz/large-datasets/csv-files-for-download/ - URL TO DOWNLOAD FILE
+        // https://ps.uci.edu/~franklin/doc/file_upload.html - URL TO UPLOAD FILE
+        // https://checkcps.com/double-click/ - URL TO DOUBLE CLICK
+        Selenide.open("https://owu.com.ua/");
     }
 
-    @AfterMethod
-    public void logout() {
-        System.out.println("This method works after each test method");
-    }
-
-    @AfterClass
-    public void closeBrowser() {
-        System.out.println("This method close browser");
-    }
-
-    @AfterGroups
-    public void afterGroup() {
-        System.out.println("This method works after group");
-    }
+//    @AfterMethod
+//    public void cleanWebDriver() {
+//        Selenide.clearBrowserCookies();
+//        Selenide.refresh();
+//        Selenide.open("about:blank");
+//    }
+//
+//    @AfterClass
+//    public void tearDown() {
+//        Selenide.closeWebDriver();
+//    }
 
 }
